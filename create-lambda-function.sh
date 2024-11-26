1c2f2360799c2757062ac8356ba1917b82daf150
Andy Luis <mvpjava.email@gmail.com>
2024-11-26 12:50:59 +0000

    added error handing and calling other script to get lastest lambda version number

--- create-lambda-function.sh
+++ create-lambda-function.sh
@@ -5,12 +5,28 @@ echo "Starting $0"
 REGION=eu-west-2
 LAMBDA_FUNCTION_NAME=myHelloWorldLambda
 
-aws lambda create-function \
+OUTPUT=$(aws lambda create-function \
 	--function-name $LAMBDA_FUNCTION_NAME \
 	--runtime python3.12 \
 	--handler myHelloWorldLambda.lambda_handler \
 	--role arn:aws:iam::403177882230:role/myLambdaAdminRoleForAllDemos \
         --code S3Bucket=codedeploy-lambda,S3Key=myHelloWorldLambda-cli/myHelloWorldLambda.zip \
-	--region $REGION
+	--region $REGION 2>&1)
+
+# Check if the creation failed due to the function already existing
+if [[ $? -ne 0 ]]; then
+    if echo "$OUTPUT" | grep -q "ResourceConflictException"; then
+        echo "Function $LAMBDA_FUNCTION_NAME already exists. Continuing without exiting..."
+    else
+        echo "An error occurred: $OUTPUT"
+        exit 1
+    fi
+else
+    echo "Function $LAMBDA_FUNCTION_NAME created successfully."
+fi 
+
+#Get latest published version number (needed for AWS CodeDeploy afterwards)
+LATEST_VERSION=$(bash ./get-latest-version-for-lambda.sh $LAMBDA_FUNCTION_NAME)
+echo "LATEST_VERSION = $LATEST_VERSION"
 
 echo "Exiting $0"
