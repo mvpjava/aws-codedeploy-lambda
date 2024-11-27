bde2c301900d223a9aefb26899021e37d1017886
Andy Luis <mvpjava.email@gmail.com>
2024-11-27 17:38:51 +0000

    now need to pass in name of function as param.

--- create-lambda-function.sh
+++ create-lambda-function.sh
@@ -3,23 +3,37 @@
 #Used in class demo with AWS CodeDeploy. This is why some of it is hardcoded.
 echo "Starting $0"
 
-REGION=eu-west-2
-LAMBDA_FUNCTION_NAME=myHelloWorldLambda
+# Usage message
+usage() {
+    echo "Usage: $0 <Lambda Function Name>"
+    echo
+    echo "Creates a Lambda function but does not publish a version"
+    echo "  <Lambda Function Name>  : The name of the Lambda function (required)"
+    exit 1
+}
+
+# Check required parameters
+if [ -z "$1" ] ; then
+    echo "Error: Missing required parameters."
+    usage
+fi
+
+LAMBDA_FUNCTION_NAME=$1
 
 OUTPUT=$(aws lambda create-function \
 	--function-name $LAMBDA_FUNCTION_NAME \
 	--runtime python3.12 \
 	--handler myHelloWorldLambda.lambda_handler \
 	--role arn:aws:iam::403177882230:role/myLambdaAdminRoleForAllDemos \
-        --code S3Bucket=codedeploy-lambda,S3Key=myHelloWorldLambda-cli/myHelloWorldLambda.zip \
-	--region $REGION 2>&1)
+        --code S3Bucket=codedeploy-lambda,S3Key=myHelloWorldLambda-cli/myHelloWorldLambda.zip 2>&1)
 
 # Check if the creation failed due to the function already existing
 if [[ $? -ne 0 ]]; then
     if echo "$OUTPUT" | grep -q "ResourceConflictException"; then
-        echo "Function $LAMBDA_FUNCTION_NAME already exists. Continuing without exiting..."
+        echo "Error: A Lambda function with the name '$LAMBDA_FUNCTION_NAME' already exists."
+	exit 1
     else
-        echo "An error occurred: $OUTPUT"
+        echo "An unexpected error occurred: $OUTPUT"
         exit 1
     fi
 else
